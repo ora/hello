c870dcadaec079d876939cae65a72fff59b7d54b
root <root@HMQX333.corp.proofpoint.com>
2022-09-26 08:54:28 -0400

    root - Mon Sep 26 08:54:28 EDT 2022

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__, template_folder='.')
 
 def hello_json():
     host = socket.gethostname()
-    platform = (platform.system()) + " - " + (platform.release()) + " - " + (platform.version()) + " - " + (platform.platform())
+    plat = (platform.system()) + " - " + (platform.release()) + " - " + (platform.version()) + " - " + (platform.platform())
     ip_addr = request.remote_addr
     user_agent = request.headers.get('User-Agent')
     local_ip = socket.gethostbyname(socket.gethostname())
@@ -17,7 +17,7 @@ def hello_json():
     if(request.method == 'GET'):
         data = {
             "Host" : host,
-            "Platform" : platform,
+            "Platform" : plat,
             "Local IP" : local_ip,
             "Remote IP" : ip_addr,
             "User Agent" : user_agent
@@ -37,5 +37,5 @@ def hello():
     return render_template('index.html', host=host, platform=plat, ip_addr=ip_addr, user_agent=user_agent, local_ip=local_ip)
 
 if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port=8080, debug=True)
+#   app.run(host="0.0.0.0", port=8080, debug=True)
     app.run(debug=True)
