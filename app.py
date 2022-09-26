2ff2fb581097e98c4afcaadb062bb1a70d783641
root <root@HMQX333.corp.proofpoint.com>
2022-09-25 22:10:34 -0400

    root - Sun Sep 25 22:10:34 EDT 2022

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template, request, jsonify
-
-import os
 import socket
 import platform
+
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, template_folder='.')
 
 @app.route('/json', methods = ['GET'])
 
-def helloJSON():
+def hello_json():
     host = socket.gethostname()
-    os = (platform.system()) + " - " + (platform.release()) + " - " + (platform.version()) + " - " + (platform.platform())
+    platform = (platform.system()) + " - " + (platform.release()) + " - " + (platform.version()) + " - " + (platform.platform())
     ip_addr = request.remote_addr
     user_agent = request.headers.get('User-Agent')
     local_ip = socket.gethostbyname(socket.gethostname())
@@ -18,7 +17,7 @@ def helloJSON():
     if(request.method == 'GET'):
         data = {
             "Host" : host,
-            "Platform" : os,
+            "Platform" : platform,
             "Local IP" : local_ip,
             "Remote IP" : ip_addr,
             "User Agent" : user_agent
@@ -30,12 +29,12 @@ def helloJSON():
 
 def hello():
     host = socket.gethostname()
-    os = (platform.system()) + " - " + (platform.release()) + " - " + (platform.version()) + " - " + (platform.platform())
+    plat = (platform.system()) + " - " + (platform.release()) + " - " + (platform.version()) + " - " + (platform.platform())
     ip_addr = request.remote_addr
     user_agent = request.headers.get('User-Agent')
     local_ip = socket.gethostbyname(socket.gethostname())
 
-    return render_template('index.html', host=host, os=os, ip_addr=ip_addr, user_agent=user_agent, local_ip=local_ip)
+    return render_template('index.html', host=host, platform=plat, ip_addr=ip_addr, user_agent=user_agent, local_ip=local_ip)
 
 if __name__ == "__main__":
 #    app.run(host="0.0.0.0", port=8080, debug=True)
